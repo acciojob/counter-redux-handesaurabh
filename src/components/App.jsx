6dd5d9bfada0f61ab8c9abd8828008a5b1910b62
handesaurabh <handesaurabh2004@gmail.com>
2025-12-17 21:43:05 +0530

    Fix button text to lowercase and add multiple click option to resolve test issues

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,8 +11,8 @@ function App() {
       <h1>{count}</h1>
       <div>
         <span></span>
-        <button onClick={() => dispatch(increment())}>Increment</button>
-        <button onClick={() => dispatch(decrement())}>Decrement</button>
+        <button onClick={() => dispatch(increment())}>increment</button>
+        <button onClick={() => dispatch(decrement())}>decrement</button>
       </div>
     </>
   )
