f34598fe3a0797ae174dc60953a7bf0028d0ae4b
handesaurabh <handesaurabh2004@gmail.com>
2025-12-17 21:17:43 +0530

    Fix import paths and remove missing CSS import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import App from './components/App.jsx'
 import store from './store.js'
 import { Provider } from 'react-redux'
 
