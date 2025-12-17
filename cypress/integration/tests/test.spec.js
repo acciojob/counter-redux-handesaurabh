23b21dcdaa79d7eb75ca3b8240a20b58ddab994a
handesaurabh <handesaurabh2004@gmail.com>
2025-12-17 21:03:40 +0530

    Add Cypress testing framework and test cases for counter app requirements

---
+++ cypress/integration/tests/test.spec.js
@@ -0,0 +1,40 @@
+describe('Counter App', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('displays initial count of 0 in h1 element', () => {
+    cy.get('h1').should('contain.text', '0')
+  })
+
+  it('has increment button as second child of div', () => {
+    cy.get('div').eq(0).children().eq(1).should('contain.text', 'increment')
+  })
+
+  it('has decrement button as third child of div', () => {
+    cy.get('div').eq(0).children().eq(2).should('contain.text', 'decrement')
+  })
+
+  it('increments counter when increment button is clicked', () => {
+    cy.get('button').contains('increment').click()
+    cy.get('h1').should('contain.text', '1')
+  })
+
+  it('decrements counter when decrement button is clicked', () => {
+    cy.get('button').contains('decrement').click()
+    cy.get('h1').should('contain.text', '-1')
+  })
+
+  it('correctly updates count after multiple clicks', () => {
+    // Click increment twice
+    cy.get('button').contains('increment').click()
+    cy.get('button').contains('increment').click()
+    cy.get('h1').should('contain.text', '2')
+    
+    // Click decrement three times
+    cy.get('button').contains('decrement').click()
+    cy.get('button').contains('decrement').click()
+    cy.get('button').contains('decrement').click()
+    cy.get('h1').should('contain.text', '-1')
+  })
+})
