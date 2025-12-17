6ba2feedcc0d86e8d5ed47a4a8e9262a8ae66ebd
handesaurabh <handesaurabh2004@gmail.com>
2025-12-17 23:26:29 +0530

    Update to use data-testid attributes as shown in example

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,28 @@ import { increment, decrement } from '../state/counterSlice'
 
 function App() {
   const count = useSelector((state) => state.counter.value)
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   return (
-    <div>
-      <h1>{count}</h1>
-      <button onClick={() => dispatch(increment())}>increment</button>
-      <button onClick={() => dispatch(decrement())}>decrement</button>
-    </div>
+    <>
+      <h1 data-testid="count">{count}</h1>
+
+      <div>
+        <button
+          data-testid="increment-btn"
+          onClick={() => dispatch(increment())}
+        >
+          increment
+        </button>
+
+        <button
+          data-testid="decrement-btn"
+          onClick={() => dispatch(decrement())}
+        >
+          decrement
+        </button>
+      </div>
+    </>
   )
 }
 
