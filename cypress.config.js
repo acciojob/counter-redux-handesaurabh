877083abe214ac74c595fdcbc2420237382c90ce
handesaurabh <handesaurabh2004@gmail.com>
2025-12-17 21:54:53 +0530

    Update Cypress config to use correct port 8082

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8080',
+    baseUrl: 'http://localhost:8082',
     supportFile: false,
     specPattern: 'cypress/integration/tests/**/*.spec.js',
     setupNodeEvents(on, config) {
